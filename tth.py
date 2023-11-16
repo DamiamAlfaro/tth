e8418a73bafd1d8f6dd9da520f2060756cfbc12a
Damiam Alfaro <149447958+DamiamAlfaro@users.noreply.github.com>
2023-11-16 11:11:10 -0800

    Add files via upload

--- tth.py
+++ tth.py
@@ -3,6 +3,40 @@ import os
 import matplotlib.pyplot as plt
 
 
+def tth_b(self): #past b) here we extract the integers representing the quantities of each instance to later graph them
+    integers = []
+    finish_time = []
+    outsets = []
+    finishes = []
+    with open(self,"r") as counts:
+        ins = counts.readlines()
+        for out,fin in enumerate(ins):
+            if out % 2 == 0:
+                outsets.append(int(eval(fin[1:-1])[3])+round(int(eval(fin[1:-1])[4])/60,3))
+            else:
+                finishes.append(int(eval(fin[1:-1])[3])+round(int(eval(fin[1:-1])[4])/60,3))
+                finish_time.append(int(eval(fin[1:-1])[3])+round(int(eval(fin[1:-1])[4])/60,2))
+
+
+    for initial,last in zip(outsets,finishes):
+        integer = round(last - initial,3)
+        integers.append(integer)
+
+    return integers,finish_time
+        
+
+def tth_c(self): #part c) graph the array of integers (amount of hours spend)
+    x = self[0]
+    y = self[1]
+    totalh = round(sum(x),2)
+    plt.scatter(x,y,color = "#093b5d")
+    plt.title(f"Hours = {totalh}")
+    plt.ylabel("Time of day")
+    plt.xlabel("Hours")
+    plt.show()
+
+    
+
 def tth_a(): #part a) this function prepares to start; checks for errors (closed or open instances), returns the last instance quantity, and annotates the instance
     dat = str(datetime.datetime.now()).split(" ")
     calendar = dat[0].split("-")
@@ -27,7 +61,7 @@ def tth_a(): #part a) this function prepares to start; checks for errors (closed
                 th = finish_time - outset_time 
                 print(f"\nLast flow = {round(th,2)} hours\n")
 
-    the = input("\n[OUTSET] [HALT]\n\n-> ")
+    the = input("\n[OUTSET] [HALT] [VIEW]\n\n-> ")
     match str(the).upper():
         case "OUTSET":
             with open("tth.txt","r+") as file:
@@ -43,57 +77,14 @@ def tth_a(): #part a) this function prepares to start; checks for errors (closed
                      raise TypeError("Open your counting dumbass.")
                 else:
                     file.write("1"+str(outset)+"\n")
+        case "VIEW":
+            tth_c(tth_b("tth.txt"))
         case _:
             print("\nWRONG WORD\n")
 
 
-
-def tth_b(self): #past b) here we extract the integers representing the quantities of each instance to later graph them
-    integers = []
-    finish_time = []
-    outsets = []
-    finishes = []
-    with open(self,"r") as counts:
-        ins = counts.readlines()
-        for out,fin in enumerate(ins):
-            if out % 2 == 0:
-                outsets.append(int(eval(fin[1:-1])[3])+round(int(eval(fin[1:-1])[4])/60,3))
-            else:
-                finishes.append(int(eval(fin[1:-1])[3])+round(int(eval(fin[1:-1])[4])/60,3))
-                finish_time.append(int(eval(fin[1:-1])[3])+round(int(eval(fin[1:-1])[4])/60,2))
-
-
-    for initial,last in zip(outsets,finishes):
-        integer = round(last - initial,3)
-        integers.append(integer)
-    
-    
-
-    return integers,finish_time
-        
-
-def tth_c(self): #part c) graph the array of integers (amount of hours spend)
-    x = self[0]
-    y = self[1]
-    totalh = round(sum(x),2)
-    plt.scatter(x,y,color = "#093b5d")
-    plt.title(f"Hours = {totalh}")
-    plt.ylabel("Time of day")
-    plt.xlabel("Hours")
-    plt.show()
-
-                    
-        
-    
-
-
-
-
-
-
 if __name__ == "__main__":
     tth_a()
-    print(tth_c(tth_b("tth.txt")))
  
 
 
